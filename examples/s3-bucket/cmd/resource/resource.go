517be2051899eff6529536a31dacd59af90ca7da
Brian Terry <brianter@amazon.com>
2020-02-20 18:31:40 -0500

    Implement the create handler

--- examples/s3-bucket/cmd/resource/resource.go
+++ examples/s3-bucket/cmd/resource/resource.go
@@ -2,122 +2,151 @@ package resource
 
 import (
 	"errors"
+	"log"
+	"os"
 
 	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 )
-
 
 // Create handles the Create event from the Cloudformation service.
 func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Create complete",
-            ResourceModel: currentModel,
-        }
+	var sess *session.Session
+	var svc s3iface.S3API
 
-        return response, nil
-    */
+	if len(*currentModel.BucketName) == 0 {
+		currentModel.BucketName = aws.String(req.LogicalResourceID)
+	}
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Create")
+	if os.Getenv("UNIT_TEST_TOGGLE") == "TRUE" {
+		// Use the test type.
+		svc = newMockS3()
+
+	} else {
+		// Get Session from the request.
+		sess = req.Session
+		svc = s3.New(sess)
+	}
+
+	// Create the S3 Bucket
+	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: currentModel.BucketName,
+	})
+
+	if err != nil {
+		return handler.ProgressEvent{
+			OperationStatus:  handler.Failed,
+			Message:          err.Error(),
+			HandlerErrorCode: cloudformation.HandlerErrorCodeInvalidRequest,
+		}, nil
+	}
+
+	log.Printf("Bucket %s successfully created\n", *currentModel.BucketName)
+
+	// Return a new ProgressEvent
+	p := handler.NewProgressEvent()
+	p.ResourceModel = currentModel
+	p.OperationStatus = handler.Success
+	p.Message = "Completed"
+
+	// return the status
+	return p, nil
+
 }
 
 // Read handles the Read event from the Cloudformation service.
 func Read(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Read complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "Read complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Read")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: Read")
 }
 
 // Update handles the Update event from the Cloudformation service.
 func Update(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Update complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "Update complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Update")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: Update")
 }
 
 // Delete handles the Delete event from the Cloudformation service.
 func Delete(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Delete complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "Delete complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Delete")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: Delete")
 }
 
 // List handles the List event from the Cloudformation service.
 func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "List complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "List complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: List")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: List")
 }
--- examples/s3-bucket/go.mod
+++ examples/s3-bucket/go.mod
@@ -2,4 +2,8 @@ module github.com/cloudformation-cli-go-plugin/examples/s3-bucket
 
 go 1.13
 
-require github.com/aws-cloudformation/cloudformation-cli-go-plugin v0.1.2
+require (
+	github.com/aws-cloudformation/cloudformation-cli-go-plugin v0.1.2
+	github.com/aws/aws-sdk-go v1.25.37
+	golang.org/x/net v0.0.0-20200219183655-46282727080f // indirect
+)
